f88ac79baef907568f1fa4348fbf0423caa2cc77
km-saifullah <kmsaifullah16@gmail.com>
2024-01-30 13:47:18 +0600

    router and tailwind css configuration done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import NotFound from "./pages/notfound/NotFound";
+import MainLayout from "./layout/MainLayout";
+import Signin from "./pages/signin/Signin";
+import Signup from "./pages/signup/Signup";
+import Home from "./pages/home/Home";
+
+// All Routes
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+
+      <Route element={<MainLayout />}>
+        <Route path="/home" element={<Home />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </>
+  )
+);
 
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
